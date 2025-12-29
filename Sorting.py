0dad6055c1d0b0c865aaf01fdc695b4e0484732f
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-29 17:43:49 +0530

    sorting is done

--- Sorting.py
+++ Sorting.py
@@ -1,4 +1,38 @@
-lst = [10,30,50,20,80]
+'''sort ascn'''
+# lst = [10,30,50,20,80]
+# print(lst)
+# lst.sort()
+# print(lst)
+
+'''sort desc'''
+# lst = [10,30,50,20,80]
+# print(lst)
+# lst.sort(reverse=True)
+# print(lst)
+
+'''sort ascn alphabetical'''
+# lst = ['python','java','abc','dhiraj']
+# print(lst)
+# lst.sort()
+# print(lst)
+
+'''sort ascn for heterogenous
+//-->error
+'''
+# lst = ['python',30,40,'abc','dhiraj','java','ruby']
+# print(lst)
+# lst.sort()
+# print(lst)
+
+'''Selection Sort'''
+
+lst = [10,2,30,5,1,50,22]
 print(lst)
-lst.sort()
+
+for i in range(0,len(lst)-1):
+    for j in range(i+1,len(lst)):
+        if(lst[j]<lst[i]):
+            lst[i],lst[j]=lst[j],lst[i]
+
 print(lst)
+    
