fde749a971805455d825bdeb1123cb54134e41d9
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-29 22:25:06 +0530

    salary

---
+++ Practice/Salary Deduction System.py
@@ -0,0 +1,15 @@
+salary = int(input("enter salary:"))
+late = int(input("enter late days:"))
+absent = int(input("enter absent days:"))
+
+deduction = 0
+if late > 10:
+    deduction += 10
+elif late > 5:
+    deduction += 5
+
+if absent > 2:
+    deduction += 5
+
+final_salary = salary - (salary * deduction / 100)
+print(int(final_salary))
